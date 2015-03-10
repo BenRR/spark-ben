8972ce76b1ab0b937234363f26be841c1775706e
Ben <bin.kuai@gmail.com>
2015-03-10 21:32:31 +1100

    upgrade to latest assembly

--- project/SparkbenBuild.scala
+++ project/SparkbenBuild.scala
@@ -1,40 +1,16 @@
 import sbt._
 import sbt.Keys._
+
 object AssemblySettings {
-    import sbtassembly.Plugin._
-     import AssemblyKeys._
-     lazy val sbtAssemblySettings = assemblySettings ++ Seq(
-
-       // Slightly cleaner jar name
-       jarName in assembly := {
-         name.value + "-" + version.value + ".jar"
-       },
-
-       // Drop these jars
-       excludedJars in assembly <<= (fullClasspath in assembly) map { cp =>
-         val excludes = Set(
-           "jsp-api-2.1-6.1.14.jar",
-           "jsp-2.1-6.1.14.jar",
-           "jasper-compiler-5.5.12.jar",
-           "commons-beanutils-core-1.8.0.jar",
-           "commons-beanutils-1.7.0.jar",
-           "servlet-api-2.5-20081211.jar",
-           "servlet-api-2.5.jar"
-         )
-         cp filter { jar => excludes(jar.data.getName) }
-       },
-
-       mergeStrategy in assembly <<= (mergeStrategy in assembly) {
-         (old) => {
-           // case "project.clj" => MergeStrategy.discard // Leiningen build files
-           case x if x.startsWith("META-INF") => MergeStrategy.discard // Bumf
-           case x if x.endsWith(".html") => MergeStrategy.discard // More bumf
-           case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last // For Log$Logger.class
-           case x => old(x)
-         }
-       }
-     )
+  import sbtassembly.AssemblyPlugin._
+  import sbtassembly.AssemblyKeys._
+  lazy val sbtAssemblySettings = assemblySettings ++ Seq(
+    jarName in assembly := {
+      name.value + "-" + version.value + ".jar"
+    }
+  )
 }
+
 object SparkBenBuild extends Build {
 
   val sparkCore = "org.apache.spark"%%"spark-core"%"1.2.0"% "provided"
@@ -44,9 +20,9 @@ object SparkBenBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "spark-ben",
       organization := "com.melbourne.ben",
-      version := "0.1-SNAPSHOT",
+      version := "0.0.1",
       scalaVersion := "2.10.2",
       libraryDependencies ++= Seq(sparkCore)
-    ) ++ AssemblySettings.sbtAssemblySettings
+    ) ++ AssemblySettings.sbtAssemblySettings 
   )
 }
